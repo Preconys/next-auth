8c11dd908d88ab5d2e52193e83823dd813d92704
Preconys <137891878+Preconys@users.noreply.github.com>
2023-12-05 13:20:37 +0100

    base path

--- packages/next-auth/src/utils/detect-origin.ts
+++ packages/next-auth/src/utils/detect-origin.ts
@@ -2,7 +2,7 @@
 export function detectOrigin(forwardedHost: any, protocol: any) {
   // If we detect a Vercel environment, we can trust the host
   if (process.env.VERCEL ?? process.env.AUTH_TRUST_HOST)
-    return `${protocol === "http" ? "http" : "https"}://${forwardedHost}`
+    return `${protocol === "http" ? "http" : "https"}://${forwardedHost}/espace-pro`
 
   // If `NEXTAUTH_URL` is `undefined` we fall back to "http://localhost:3000"
   return process.env.NEXTAUTH_URL
